6082b13184adbc67787f8a42b8c620861efb68b4
Zuzana Dostalova <dostalovaz@gmail.com>
2017-03-25 04:11:22 +0100

    Rewritting conversation starters to Czech

--- facebook_bot.js
+++ facebook_bot.js
@@ -1,71 +1,3 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Facebook bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Facebook's Messenger APIs
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
-# RUN THE BOT:
-
-  Follow the instructions here to set up your Facebook app and page:
-
-    -> https://developers.facebook.com/docs/messenger-platform/implementation
-
-  Run your bot from the command line:
-
-    app_secret=<MY APP SECRET> page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node facebook_bot.js [--lt [--ltsubdomain LOCALTUNNEL_SUBDOMAIN]]
-
-  Use the --lt option to make your bot available on the web through localtunnel.me.
-
-# USE THE BOT:
-
-  Find your bot inside Facebook to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "who are you?"
-
-  The bot will tell you its name, where it running, and for how long.
-
-  Say: "Call me <nickname>"
-
-  Tell the bot your nickname. Now you are friends.
-
-  Say: "who am I?"
-
-  The bot will tell you your nickname, if it knows one for you.
-
-  Say: "shutdown"
-
-  The bot will ask if you are sure, and then shut itself down.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-
-# EXTEND THE BOT:
-
-  Botkit has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
 if (!process.env.page_token) {
     console.log('Error: Specify page_token in environment');
     process.exit(1);
@@ -168,156 +100,28 @@ controller.api.messenger_profile.menu([{
     }
 ]);
 
-// controller.api.messenger_profile.account_linking('https://www.yourAwesomSite.com/oauth?response_type=code&client_id=1234567890&scope=basic');
-// controller.api.messenger_profile.get_account_linking(function (err, accountLinkingUrl)  {
-//     console.log('****** Account linkink URL :', accountLinkingUrl);
-// });
-// controller.api.messenger_profile.delete_account_linking();
-// controller.api.messenger_profile.domain_whitelist('https://localhost');
-// controller.api.messenger_profile.domain_whitelist(['https://127.0.0.1', 'https://0.0.0.0']);
-// controller.api.messenger_profile.delete_domain_whitelist('https://localhost');
-// controller.api.messenger_profile.delete_domain_whitelist(['https://127.0.0.1', 'https://0.0.0.0']);
-// controller.api.messenger_profile.get_domain_whitelist(function (err, data)  {
-//     console.log('****** Whitelisted domains :', data);
-// });
+///////////////////////////////
+// OUR CODEBASE ///////////////
+///////////////////////////////
 
-
-// returns the bot's messenger code image
-controller.hears(['code'], 'message_received,facebook_postback', function(bot, message) {
-    controller.api.messenger_profile.get_messenger_code(2000, function (err, url) {
-        if(err) {
-            // Error
-        } else {
-            var image = {
-                "attachment":{
-                    "type":"image",
-                    "payload":{
-                        "url": url
-                    }
-                }
-            };
-            bot.reply(message, image);
-        }
-    });
-});
-
-controller.hears(['quick'], 'message_received', function(bot, message) {
-
-    bot.reply(message, {
-        text: 'Hey! This message has some quick replies attached.',
-        quick_replies: [
-            {
-                "content_type": "text",
-                "title": "Yes",
-                "payload": "yes",
-            },
-            {
-                "content_type": "text",
-                "title": "No",
-                "payload": "no",
-            }
-        ]
-    });
-
-});
-
-controller.hears(['^hello', '^hi'], 'message_received,facebook_postback', function(bot, message) {
+///////////////////////////////
+// START OF THE CONVERSATION //
+///////////////////////////////
+controller.hears(['^ahoj', '^čau', '^cau', '^zdravim', '^nazdar', '^hoj'], 'message_received,facebook_postback', function(bot, message) {
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
+            bot.reply(message, 'Ahoj ' + user.name + '!!');
         } else {
-            bot.reply(message, 'Hello.');
+            bot.reply(message, 'Ahoj.');
         }
     });
 });
 
-controller.hears(['silent push reply'], 'message_received', function(bot, message) {
-    reply_message = {
-        text: "This message will have a push notification on a mobile phone, but no sound notification",
-        notification_type: "SILENT_PUSH"
-    }
-    bot.reply(message, reply_message)
-})
-
-controller.hears(['no push'], 'message_received', function(bot, message) {
-    reply_message = {
-        text: "This message will not have any push notification on a mobile phone",
-        notification_type: "NO_PUSH"
-    }
-    bot.reply(message, reply_message)
-})
-
-controller.hears(['structured'], 'message_received', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-        convo.ask({
-            attachment: {
-                'type': 'template',
-                'payload': {
-                    'template_type': 'generic',
-                    'elements': [
-                        {
-                            'title': 'Classic White T-Shirt',
-                            'image_url': 'http://petersapparel.parseapp.com/img/item100-thumb.png',
-                            'subtitle': 'Soft white cotton t-shirt is back in style',
-                            'buttons': [
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=100',
-                                    'title': 'View Item'
-                                },
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=100',
-                                    'title': 'Buy Item'
-                                },
-                                {
-                                    'type': 'postback',
-                                    'title': 'Bookmark Item',
-                                    'payload': 'White T-Shirt'
-                                }
-                            ]
-                        },
-                        {
-                            'title': 'Classic Grey T-Shirt',
-                            'image_url': 'http://petersapparel.parseapp.com/img/item101-thumb.png',
-                            'subtitle': 'Soft gray cotton t-shirt is back in style',
-                            'buttons': [
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=101',
-                                    'title': 'View Item'
-                                },
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=101',
-                                    'title': 'Buy Item'
-                                },
-                                {
-                                    'type': 'postback',
-                                    'title': 'Bookmark Item',
-                                    'payload': 'Grey T-Shirt'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        }, function(response, convo) {
-            // whoa, I got the postback payload as a response to my convo.ask!
-            convo.next();
-        });
-    });
+controller.hears(['^hi', '^hello', 'how are you'], 'message_received,facebook_postback', function(bot, message) {
+  bot.reply(message, 'Mluv prosimtě česky, jo?');
 });
 
-controller.on('facebook_postback', function(bot, message) {
-    // console.log(bot, message);
-   bot.reply(message, 'Great Choice!!!! (' + message.payload + ')');
-
-});
-
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'message_received', function(bot, message) {
+controller.hears(['[rř][ií]kej mi (.*)', 'jmenuj[ui] se (.*)'], 'message_received', function(bot, message) {
     var name = message.match[1];
     controller.storage.users.get(message.user, function(err, user) {
         if (!user) {
@@ -327,23 +131,23 @@ controller.hears(['call me (.*)', 'my name is (.*)'], 'message_received', functi
         }
         user.name = name;
         controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+            bot.reply(message, 'Jasně, budu ti říkat ' + user.name);
         });
     });
 });
 
-controller.hears(['what is my name', 'who am i'], 'message_received', function(bot, message) {
+controller.hears(['jak se jmenuj[ui]', 'who am i'], 'message_received', function(bot, message) {
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
+            bot.reply(message, 'Jsi ' + user.name);
         } else {
             bot.startConversation(message, function(err, convo) {
                 if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
+                    convo.say('To přece ještě nevim!');
+                    convo.ask('Jak ti mám říkat?', function(response, convo) {
+                        convo.ask('Chceš abych ti říkal `' + response.text + '`?', [
                             {
-                                pattern: 'yes',
+                                pattern: 'ano',
                                 callback: function(response, convo) {
                                     // since no further messages are queued after this,
                                     // the conversation will end naturally with status == 'completed'
@@ -351,7 +155,7 @@ controller.hears(['what is my name', 'who am i'], 'message_received', function(b
                                 }
                             },
                             {
-                                pattern: 'no',
+                                pattern: 'ne',
                                 callback: function(response, convo) {
                                     // stop the conversation. this will cause it to end with status == 'stopped'
                                     convo.stop();
@@ -372,7 +176,7 @@ controller.hears(['what is my name', 'who am i'], 'message_received', function(b
 
                     convo.on('end', function(convo) {
                         if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
+                            bot.reply(message, 'OK! Budu si to pamatovat.');
 
                             controller.storage.users.get(message.user, function(err, user) {
                                 if (!user) {
@@ -382,7 +186,7 @@ controller.hears(['what is my name', 'who am i'], 'message_received', function(b
                                 }
                                 user.name = convo.extractResponse('nickname');
                                 controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                    bot.reply(message, 'Jasan, budu ti říkat ' + user.name);
                                 });
                             });
 
@@ -390,7 +194,7 @@ controller.hears(['what is my name', 'who am i'], 'message_received', function(b
 
                         } else {
                             // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
+                            bot.reply(message, 'OK, nevadí!');
                         }
                     });
                 }
@@ -403,11 +207,11 @@ controller.hears(['shutdown'], 'message_received', function(bot, message) {
 
     bot.startConversation(message, function(err, convo) {
 
-        convo.ask('Are you sure you want me to shutdown?', [
+        convo.ask('Opravdu?', [
             {
                 pattern: bot.utterances.yes,
                 callback: function(response, convo) {
-                    convo.say('Bye!');
+                    convo.say('Ahoj!');
                     convo.next();
                     setTimeout(function() {
                         process.exit();
@@ -427,20 +231,20 @@ controller.hears(['shutdown'], 'message_received', function(bot, message) {
 });
 
 
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'message_received',
+controller.hears(['uptime', 'p[rř]edstav se', 'kdo jsi', 'jak se jmenuje[sš]'], 'message_received',
     function(bot, message) {
 
         var hostname = os.hostname();
         var uptime = formatUptime(process.uptime());
 
         bot.reply(message,
-            ':|] I am a bot. I have been running for ' + uptime + ' on ' + hostname + '.');
+            ':|] Jsem bot a běžím už ' + uptime + ' na ' + hostname + '.');
     });
 
 
 
 controller.on('message_received', function(bot, message) {
-    bot.reply(message, 'Try: `what is my name` or `structured` or `call me captain`');
+    bot.reply(message, 'Zkus: jak se jmenuju, ahoj, kdo jsi nebo říkej mi (*)');
     return false;
 });
 
